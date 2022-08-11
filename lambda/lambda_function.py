14ac29ebcc1a89cc469213fc0951ed9c6b509681
Param Bidja <parambidja@gmail.com>
2022-08-10 22:48:49 -0400

    test commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,7 +3,7 @@
  You may use, distribute and modify this code under the
  terms and conditions defined in file 'LICENSE.txt', which
  is part of this source code package.
- 
+
  For additional copyright information please
  visit : http://dabblelab.com/copyright
  """
@@ -20,6 +20,8 @@ import json
 import random
 import os
 
+test = "test string"
+
 s3_adapter = S3Adapter(bucket_name = os.environ.get("S3_PERSISTENCE_BUCKET"))
 
 # Initializing the logger and setting the level to "INFO"
@@ -31,45 +33,45 @@ logger.setLevel(logging.INFO)
 
 # This handler responds when required environment variables are missing
 class InvalidConfigHandler(AbstractRequestHandler):
-    
+
     def can_handle(self, handler_input):
         request_attributes = handler_input.attributes_manager.request_attributes
         invalid_config = request_attributes["invalid_config"]
         return invalid_config
-        
+
     def handle(self,handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         speech_output = language_prompts["ENV_NOT_CONFIGURED"]
-        return ( 
+        return (
             handler_input.response_builder
                 .speak(speech_output)
                 .response )
 
 #This Handler is called when the skill is invoked by using only the invocation name(Ex. Alexa, open template four)
 class LaunchRequestHandler(AbstractRequestHandler):
-    
+
     def can_handle(self, handler_input):
         return is_request_type("LaunchRequest")(handler_input)
-    
+
     def handle(self, handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         persistent_attributes = handler_input.attributes_manager.persistent_attributes
         session_attributes = handler_input.attributes_manager.session_attributes
-        
+
         try:
             user_name = persistent_attributes["user_name"]
         except:
             user_name = None
-        
+
         skill_name = language_prompts["SKILL_NAME"]
-        
+
         if user_name is not None:
             speech_output = random.choice(language_prompts["GREETING"]).format(user_name)
             reprompt = random.choice(language_prompts["GREETING_REPROMPT"])
         else:
             speech_output = random.choice(language_prompts["GREETING_UNKNOWN_USER"]).format(skill_name)
             reprompt = random.choice(language_prompts["GREETING_UNKNOWN_USER_REPROMPT"])
-        
+
         return (
             handler_input.response_builder
                 .speak(speech_output)
@@ -81,16 +83,16 @@ class LaunchRequestHandler(AbstractRequestHandler):
 class MyNameIsIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("MyNameIsIntent")(handler_input)
-    
+
     def handle(self,handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         user_name = handler_input.request_envelope.request.intent.slots["name"].value
-        
+
         speech_output = random.choice(language_prompts["GREETING_RESPONSE"]).format(user_name)
-        
+
         handler_input.attributes_manager.persistent_attributes["user_name"] = user_name
         handler_input.attributes_manager.save_persistent_attributes()
-        
+
         return(
             handler_input.response_builder
                 .speak(speech_output)
@@ -100,10 +102,10 @@ class MyNameIsIntentHandler(AbstractRequestHandler):
 class LearnMoreIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("LearnMoreIntent")(handler_input)
-    
+
     def handle(self, handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
-        
+
         speech_output = random.choice(language_prompts["LEARN_MORE"])
         return (
             handler_input.response_builder
@@ -115,17 +117,17 @@ class LearnMoreIntentHandler(AbstractRequestHandler):
 class RepeatIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("AMAZON.RepeatIntent")(handler_input)
-    
+
     def handle(self, handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         session_attributes = handler_input.attributes_manager.session_attributes
-        
+
         repeat_speech_output = session_attributes["repeat_speech_output"]
         repeat_reprompt = session_attributes["repeat_reprompt"]
-        
+
         speech_output = random.choice(language_prompts["REPEAT"]).format(repeat_speech_output)
         reprompt = random.choice(language_prompts["REPEAT_REPROMPT"]).format(repeat_reprompt)
-        
+
         return (
             handler_input.response_builder
                 .speak(speech_output)
@@ -137,16 +139,16 @@ class YesNoIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return (is_intent_name("AMAZON.YesIntent")(handler_input) or
                 is_intent_name("AMAZON.NoIntent")(handler_input))
-    
+
     def handle(self, handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         session_attributes = handler_input.attributes_manager.session_attributes
-        
+
         if is_intent_name("AMAZON.YesIntent")(handler_input):
             return LearnMoreIntentHandler.handle(handler_input)
         else:
             speech_output = random.choice(language_prompts["NO"])
-        
+
         return (
             handler_input.response_builder
                 .speak(speech_output)
@@ -157,11 +159,11 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
                 is_intent_name("AMAZON.StopIntent")(handler_input))
-    
+
     def handle(self, handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         speech_output = random.choice(language_prompts["CANCEL_STOP_RESPONSE"])
-        
+
         return (
             handler_input.response_builder
                 .speak(speech_output)
@@ -172,12 +174,12 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
 class HelpIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("AMAZON.HelpIntent")(handler_input)
-    
+
     def handle(self, handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         speech_output = random.choice(language_prompts["HELP"])
         reprompt = random.choice(language_prompts["HELP_REPROMPT"])
-        
+
         return (
             handler_input.response_builder
                 .speak(speech_output)
@@ -189,12 +191,12 @@ class HelpIntentHandler(AbstractRequestHandler):
 class FallbackIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("AMAZON.FallbackIntent")(handler_input)
-    
+
     def handle(self, handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         speech_output = random.choice(language_prompts["FALLBACK"])
         reprompt = random.choice(language_prompts["FALLBACK_REPROMPT"])
-        
+
         return (
             handler_input.response_builder
                 .speak(speech_output)
@@ -205,29 +207,29 @@ class FallbackIntentHandler(AbstractRequestHandler):
 class SessionEndedRequesthandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_request_type("SessionEndedRequest")(handler_input)
-    
+
     def handle(self, handler_input):
         logger.info("Session ended with the reason: {}".format(handler_input.request_envelope.request.reason))
         return handler_input.response_builder.response
 
 # Exception Handlers
 
-# This exception handler handles syntax or routing errors. If you receive an error stating 
-# the request handler is not found, you have not implemented a handler for the intent or 
+# This exception handler handles syntax or routing errors. If you receive an error stating
+# the request handler is not found, you have not implemented a handler for the intent or
 # included it in the skill builder below
 class CatchAllExceptionHandler(AbstractExceptionHandler):
-    
+
     def can_handle(self, handler_input, exception):
         return True
-    
+
     def handle(self, handler_input, exception):
         logger.error(exception, exc_info=True)
-        
+
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
-        
+
         speech_output = language_prompts["ERROR"]
         reprompt = language_prompts["ERROR_REPROMPT"]
-        
+
         return (
             handler_input.response_builder
                 .speak(speech_output)
@@ -267,14 +269,14 @@ class LocalizationInterceptor(AbstractRequestInterceptor):
     def process(self, handler_input):
         locale = handler_input.request_envelope.request.locale
         logger.info("Locale is {}".format(locale))
-        
+
         try:
             with open("languages/"+str(locale)+".json") as language_data:
                 language_prompts = json.load(language_data)
         except:
             with open("languages/"+ str(locale[:2]) +".json") as language_data:
                 language_prompts = json.load(language_data)
-        
+
         handler_input.attributes_manager.request_attributes["_"] = language_prompts
 
 # This interceptor fetches the speech_output and reprompt messages from the response and pass them as
